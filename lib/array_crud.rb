06f419bcb2b40531b87a568bc2e34edfeb6dc2dc
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-21 01:16:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [mother, father, daughter, son]
+  ["mother", "father", "daughter", "son"]
 end
 
 def add_element_to_end_of_array(array, element)
