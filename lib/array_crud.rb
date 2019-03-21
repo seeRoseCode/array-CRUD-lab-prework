fe3414120cbe05f84a705643817d5843d750f6a0
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-21 01:21:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  item = array[index_number]
+  return item
 end
 
 def retrieve_first_element_from_array(array)
