081501386d28b281d93477ede11f3d9bc153edb9
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-21 01:16:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  []
+  family = []
 end
 
 def create_an_array
-  ["mother", "father", "daughter", "son"]
+  family = ["mother", "father", "daughter", "son"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  family.push("aunt")
 end
 
 def add_element_to_start_of_array(array, element)
